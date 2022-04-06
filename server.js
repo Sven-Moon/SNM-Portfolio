d5ef92923267ddcad5d6d37448912aa8b5eca66e
Svenpire <svenpire@gmail.com>
2022-04-06 12:00:02 -0600

    initial structure

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+PORT = 3000;
+
+app.use(
+  "/static",
+  express.static(path.resolve(__dirname, "frontend", "static"))
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.listen(process.env.PORT || PORT, () =>
+  console.log("Server is running on port " + PORT)
+);
