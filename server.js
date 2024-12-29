17d0273fd632f2deeb411ef8a577b107bf9e3d44
Sven Moon <35192154+Sven-Moon@users.noreply.github.com>
2024-12-29 16:36:36 -0700

    Dev journey (#4)
    * redirect local server to public
    
    * update interest text with BecLS+
    
    * fix interest hero text size at trouble widths
    
    * gradient background to hero

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ PORT = 3000;
 
 app.use(
   "/static",
-  express.static(path.resolve(__dirname, "frontend", "static"))
+  express.static(path.resolve(__dirname, "public", "static"))
 );
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
 app.listen(process.env.PORT || PORT, () =>
